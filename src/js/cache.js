b9e739cdfe91a533a85a83b25c66b4794987059c
thulioph <thulioph@gmail.com>
2016-03-16 20:09:41 -0300

    Referenced manifest into html, adds script and create file with instructions for cache.

---
+++ src/js/cache.js
@@ -0,0 +1,65 @@
+var _initCache = function() {
+    var appCache, that;
+
+    that = this;
+    appCache = window.applicationCache;
+
+    appCache.addEventListener('cached', handleCachedEvent, false);
+    appCache.addEventListener('checking', handleCacheChecking, false);
+    appCache.addEventListener('downloading', handleCacheDownloading, false);
+    appCache.addEventListener('error', handleCacheError, false);
+    appCache.addEventListener('noupdate', handleCacheNoupdate, false);
+    appCache.addEventListener('obsolete', handleCacheObsolete, false);
+    appCache.addEventListener('progress', handleCacheProgress, false);
+    appCache.addEventListener('updateready', handleCacheUpdateready, false);
+};
+
+_initCache();
+
+var handleCachedEvent = function() {
+    console.log(event)
+};
+
+var handleCacheChecking = function() {
+    console.log(event)
+};
+
+var handleCacheDownloading = function() {
+    console.log(event)
+
+    new Notification('Download', {
+        body: 'Realizando o download de novos conteúdos.',
+        icon: 'assets/image/icon.png'
+    });
+};
+
+var handleCacheError = function() {
+    console.log(event)
+};
+
+var handleCacheNoupdate = function() {
+    console.log(event)
+};
+
+var handleCacheObsolete = function() {
+    console.log(event)
+};
+
+var handleCacheProgress = function() {
+    console.log(event)
+};
+
+var handleCacheUpdateready = function() {
+    var appCache;
+
+    appCache.swapCache();
+
+    // if (confirm('Existe uma nova versão para este site, deseja atualizar?')) {
+    //   window.location.reload(); // refresh page to apply new updates
+    // }
+
+    new Notification('Novidades!', {
+        body: 'Um novo vídeo foi adicionado, atualize a página para visualiza-lo :)',
+        icon: 'assets/image/icon.png'
+    });
+};
